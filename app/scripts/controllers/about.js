420c60a546fa4064d2289633ba1798bab3688827
hhsadiq <hhsadiq@gmail.com>
2015-12-18 21:12:01 +0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codelabApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the codelabApp
+ */
+angular.module('codelabApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codelabApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the codelabApp
+ */
+angular.module('codelabApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
